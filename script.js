62cfd7c80af20928b89c42b884db94d6fe7a14ec
Sam Dollar <sdollar1@gmail.com>
2021-07-30 18:27:15 -0500

    second update

--- script.js
+++ script.js
@@ -14,16 +14,21 @@ step 6 - prompt restart or exit?
 const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
+let showPlayerChoice = document.getElementById('showPlayerChoice');
+let showCompChoice = document.getElementById('showCompChoice');
+
+let playerChoice = '';
+let compChoice = '';
 
 
 function game() {
-    for(let i = 0; i <= 5; i++) {
+    for(let i = 0; i < 5; i++) {
         round();
         //this makes round play out 6 times. Needs to stop after 1 click.
     }
 }
 
-function getPlayerChoice(playerChoice) {
+function getPlayerChoice() {
     rock.addEventListener('click', function() {
         playerChoice = 'Rock'
         console.log(playerChoice);
@@ -42,9 +47,14 @@ function getPlayerChoice(playerChoice) {
 
 }
 
-function getCompChoice(compChoice) {
-
+function getCompChoice() {
+    let things = ['Rock', 'Paper', 'Scissors'];
+    compChoice = things[Math.floor(Math.random()*things.length)];
+    console.log('c'+ compChoice)
     return compChoice;
+    //right now this runs as soon as the page loads before user selects.
+    //probably reference it inside getPlayerChoice() button events so 
+    //it runs after a click.
 }
 
 function round() {
@@ -52,6 +62,16 @@ function round() {
     //of the round. Return to next part of the game loop.
     getPlayerChoice();
     getCompChoice();
+
+    if(playerChoice === rock) {
+        if(compChoice === 'paper') {
+            console.log('You lose')
+        } else if(compChoice === 'rock') {
+            console.log('Tie')
+        } else if(compChoice === 'scissors') {
+            console.log('You win')
+        }
+    }
 
     //if playerChoice beats compChoice, player wins and round is over.
     //else comp wins and round is over.
