51c4b91658bfbf50316bba7bd2ba49622156a4bf
Sam Dollar <sdollar1@gmail.com>
2021-07-31 00:39:15 -0500

    update

--- script.js
+++ script.js
@@ -4,12 +4,6 @@ enters a random choice. The two choices are compared, and a winner for
 that round is chosen. This process is done 5 times, and the side with the
 most rounds won, wins overall.
 
-step 1 - player selects choice, game loop begins (total 5)
-step 2 - comp generates random choice
-step 3 - the two choices are compared
-step 4 - round winner decided, next round begins
-step 5 - after fifth round, side with most round wins becomes overall winner
-step 6 - prompt restart or exit?
 */
 const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
@@ -19,30 +13,40 @@ let showCompChoice = document.getElementById('showCompChoice');
 
 let playerChoice = '';
 let compChoice = '';
-
+let playerWinCount = 0;
+let compWinCount = 0;
+let roundNumber = 0;
 
 function game() {
-    for(let i = 0; i < 5; i++) {
-        round();
-        //this makes round play out 6 times. Needs to stop after 1 click.
-    }
+    round();
+    return;
+
 }
 
 function getPlayerChoice() {
     rock.addEventListener('click', function() {
         playerChoice = 'Rock'
         console.log(playerChoice);
-        return playerChoice;
+        getCompChoice();
+        compareChoices(playerChoice, compChoice);
+        roundNumber++
+        return roundNumber
     })
     paper.addEventListener('click', function() {
         playerChoice = 'Paper'
         console.log(playerChoice);
-        return playerChoice;
+        getCompChoice();
+        compareChoices(playerChoice, compChoice);
+        roundNumber++
+        return roundNumber
     })
     scissors.addEventListener('click', function() {
         playerChoice = 'Scissors'
         console.log(playerChoice);
-        return playerChoice;
+        getCompChoice();
+        compareChoices(playerChoice, compChoice);
+        roundNumber++
+        return roundNumber
     })
 
 }
@@ -50,32 +54,60 @@ function getPlayerChoice() {
 function getCompChoice() {
     let things = ['Rock', 'Paper', 'Scissors'];
     compChoice = things[Math.floor(Math.random()*things.length)];
-    console.log('c'+ compChoice)
+    console.log('c'+ compChoice) //just for debugging
     return compChoice;
-    //right now this runs as soon as the page loads before user selects.
-    //probably reference it inside getPlayerChoice() button events so 
-    //it runs after a click.
+}
+
+function compareChoices(pChoice, cChoice) {
+    if(pChoice == 'Rock') {
+        if(cChoice == 'Paper') {
+            console.log('You lose')
+            compWinCount++;
+            return compWinCount
+        } else if(cChoice == 'Rock') {
+            console.log('Tie')
+        } else if(cChoice == 'Scissors') {
+            console.log('You win')
+            playerWinCount++;
+            return playerWinCount
+        }
+    } else if(pChoice == 'Paper') {
+        if(cChoice == 'Paper') {
+            console.log('Tie')
+        } else if(cChoice == 'Rock') {
+            console.log('You win')
+            playerWinCount++
+            return playerWinCount
+        } else if(cChoice == 'Scissors') {
+            console.log('You lose')
+            compWinCount++
+            return compWinCount
+        }
+    } else if(pChoice == 'Scissors') {
+        if(cChoice == 'Paper') {
+            console.log('You win')
+            playerWinCount++
+            return playerWinCount
+        } else if(cChoice == 'Rock') {
+            console.log('You lose')
+            compWinCount++
+            return compWinCount
+        } else if(cChoice == 'Scissors') {
+            console.log('Tie')
+        }
+    } //eventually update this so score on screen is updated.
+}
+
+function endGame() {
+    console.log('Game Over')
+    
 }
 
 function round() {
-    //get the two choices, display and compare them and reveal the winner
-    //of the round. Return to next part of the game loop.
-    getPlayerChoice();
-    getCompChoice();
-
-    if(playerChoice === rock) {
-        if(compChoice === 'paper') {
-            console.log('You lose')
-        } else if(compChoice === 'rock') {
-            console.log('Tie')
-        } else if(compChoice === 'scissors') {
-            console.log('You win')
-        }
+    while(roundNumber < 5) {
+        getPlayerChoice()
     }
-
-    //if playerChoice beats compChoice, player wins and round is over.
-    //else comp wins and round is over.
-    //next round (return?)
+    endGame()
 }
 
 game();
